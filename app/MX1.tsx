987a8d7a63bf7d93a1b4506db92b24aa58521e11
Senlinmusi <senlinmusi80@gmail.com>
2026-07-04 21:53:46 +0800

    更新

--- app/MX1.tsx
+++ app/MX1.tsx
@@ -4,38 +4,39 @@ import { useFrame, useThree } from '@react-three/fiber'
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 
-export default function MX1({ FX1, controlsRef }: { FX1: THREE.Vector3, controlsRef: any }) {
+export default function MX1({ FX1, CR1 }: { FX1: THREE.Vector3, CR1: any }) {
   const { scene, animations } = useGLTF('/walk.glb')
   const { actions } = useAnimations(animations, scene)
   const XR1 = useRef<THREE.Group>(null)
   const { camera } = useThree()
-  const VT1 = useRef(new THREE.Vector3(0, 0, 0))
 
-  scene.traverse((c) => {
-    if (c instanceof THREE.Mesh) {
-      c.material = new THREE.MeshToonMaterial({ color: (c.material as THREE.MeshStandardMaterial).color })
+  scene.traverse((DX1) => {
+    if (DX1 instanceof THREE.Mesh) {
+      DX1.material = new THREE.MeshToonMaterial({
+        color: (DX1.material as THREE.MeshStandardMaterial).color,
+        map: (DX1.material as THREE.MeshStandardMaterial).map
+      })
     }
   })
 
-  useFrame((state, delta) => {
+  useFrame((ZT1, SJ1) => {
     if (!XR1.current) return
 
     if (FX1.length() > 0) {
-      const angle = Math.atan2(camera.position.x - XR1.current.position.x, camera.position.z - XR1.current.position.z)
-      const move = FX1.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), angle)
-      XR1.current.position.add(move.multiplyScalar(delta * 3))
-      XR1.current.lookAt(XR1.current.position.clone().add(move))
+      const JD1 = Math.atan2(camera.position.x - XR1.current.position.x, camera.position.z - XR1.current.position.z)
+      const YD1 = FX1.clone().applyAxisAngle(new THREE.Vector3(0, 1, 0), JD1)
+      XR1.current.position.add(YD1.multiplyScalar(SJ1 * 3))
+      XR1.current.lookAt(XR1.current.position.clone().add(YD1))
     }
 
-    if (controlsRef.current) {
-      VT1.current.lerp(XR1.current.position.clone().add(new THREE.Vector3(0, 1.2, 0)), 0.1)
-      controlsRef.current.target.copy(VT1.current)
+    if (CR1.current) {
+      CR1.current.target.copy(XR1.current.position).setY(1.2)
     }
   })
 
   useEffect(() => {
-    const act = actions[Object.keys(actions)[0]]
-    FX1.length() > 0 ? act?.play() : act?.stop()
+    const DZ1 = actions[Object.keys(actions)[0]]
+    FX1.length() > 0 ? DZ1?.play() : DZ1?.stop()
   }, [FX1, actions])
 
   return <primitive ref={XR1} object={scene} scale={1.2} />
